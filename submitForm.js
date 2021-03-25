ec7207da21068dad645f851bdc6e889cec4a0a4b
Christy Cooke <cleecooke@gmail.com>
2021-03-25 15:51:34 -0600

    First Commit after moving project for Netlify

---
+++ submitForm.js
@@ -0,0 +1,16 @@
+function submitForm() {
+
+    let fName = document.getElementById("fnameInput").value,
+        lName = document.getElementById("lnameInput").value;
+        email = document.getElementById("emailInput").value;
+        phone = document.getElementById("phoneInput").value;
+        age = document.getElementById("ageInput").value;
+        message = document.getElementById("messageInput").value;
+    
+        document.getElementById("fnameOutput").value = fName;
+        document.getElementById("lnameOutput").value = lName;
+        document.getElementById("emailOutput").value = 
+        document.getElementById("phoneOutput").value
+        document.getElementById("ageOutput").value
+        document.getElementById("messageOutput").value
+}
